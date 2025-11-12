c2d025b7f0ed43d72d665e30bf85cbe7f90436ed
NeX0uS <artemkirpotenko@gmail.com>
2025-11-12 02:02:56 +0100

    server error

--- index.js
+++ index.js
@@ -18,8 +18,7 @@ const app = express()
 const port = process.env.PORT || 3000;
 const allowedOrigins = [
     'http://localhost:5173',
-    'https://myfrontend.onrender.com',
-    'skinvoltfront.onrender.com'
+    'https://skinvoltfront.onrender.com'
 ];
 
 app.use(express.json());
