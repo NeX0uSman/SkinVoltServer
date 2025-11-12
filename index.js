9795512a6c7f3fb6a8ceda146b2e365411c1ce3f
NeX0uS <artemkirpotenko@gmail.com>
2025-11-12 02:09:51 +0100

    server error

--- index.js
+++ index.js
@@ -23,8 +23,16 @@ const allowedOrigins = [
 
 app.use(express.json());
 app.use(cors({
-    origin: allowedOrigins
-}))
+  origin: function(origin, callback) {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  },
+  credentials: true,
+}));
 
 mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
